abb41d74e02b5fc5ab13c8d94eb0c1b05e1429a4
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-10-24 23:59:34 +0530

    added

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -3,6 +3,47 @@ import SliderSlick from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { assets } from "../assets/assets";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi"; // Import icons
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      style={{
+        display: "block",
+        position: "absolute",
+        top: "50%",
+        right: "10px",
+        transform: "translate(0, -50%)",
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+    >
+      <BiChevronRight size={50} color="blue" /> {/* Custom icon */}
+    </div>
+  );
+};
+
+// Custom Previous Arrow
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      style={{
+        display: "block",
+        position: "absolute",
+        top: "50%",
+        left: "10px",
+        transform: "translate(0, -50%)",
+        zIndex: 1, // Ensure it appears above the slides
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+    >
+      <BiChevronLeft size={50} color="blue" /> {/* Custom icon */}
+    </div>
+  );
+};
+
 const Slider = () => {
   var settings = {
     dots: true,
@@ -10,6 +51,8 @@ const Slider = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />, // Add custom next arrow
+    prevArrow: <PrevArrow />, // Add custom prev arrow
   };
 
   return (
