85bab85deccbbf1ab8cc38d1dc3c6857305c6481
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-11-01 20:23:00 +0530

    added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,6 +10,8 @@ const ShopContextProvider = (props) => {
     const currency = '';
     const delivery_fee = 10;
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+    console.log(backendUrl);
+    
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
