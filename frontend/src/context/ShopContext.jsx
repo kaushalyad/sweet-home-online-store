249b4880c4979a3efdfb0b07cfa166296ea15af1
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-11-02 08:22:52 +0530

    dkj

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,8 +9,8 @@ const ShopContextProvider = (props) => {
 
     const currency = '';
     const delivery_fee = 10;
-    console.log(import.meta.env.VITE_BACKEND_URL);
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    // console.log(import.meta.env.VITE_BACKEND_URL);
+    const backendUrl = 'https://api.sweethome-store.com/api/product/list'
 
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
