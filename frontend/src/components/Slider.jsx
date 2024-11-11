a724885615c0ee6dd0e75ef621a1fa1ee0511ac1
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-11-12 01:02:18 +0530

    added slider

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -3,7 +3,8 @@ import SliderSlick from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { assets } from "../assets/assets";
-import { BiChevronLeft, BiChevronRight } from "react-icons/bi"; // Import icons
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+
 const NextArrow = (props) => {
   const { onClick } = props;
   return (
@@ -12,18 +13,22 @@ const NextArrow = (props) => {
         display: "block",
         position: "absolute",
         top: "50%",
-        right: "10px",
+        right: "1px",
         transform: "translate(0, -50%)",
         cursor: "pointer",
+        backgroundColor: "black",
+        padding: "22px 2px",
+        borderTopLeftRadius: "8px",
+        borderBottomLeftRadius: "8px",
+        zIndex: 2,
       }}
       onClick={onClick}
     >
-      <BiChevronRight size={40} color="blue" /> {/* Custom icon */}
+      <BiChevronRight size={30} color="blue" />
     </div>
   );
 };
 
-// Custom Previous Arrow
 const PrevArrow = (props) => {
   const { onClick } = props;
   return (
@@ -32,14 +37,18 @@ const PrevArrow = (props) => {
         display: "block",
         position: "absolute",
         top: "50%",
-        left: "10px",
+        left: "1px",
         transform: "translate(0, -50%)",
-        zIndex: 1, // Ensure it appears above the slides
         cursor: "pointer",
+        backgroundColor: "black",
+        padding: "22px 2px",
+        borderTopRightRadius: "8px",
+        borderBottomRightRadius: "8px",
+        zIndex: 2,
       }}
       onClick={onClick}
     >
-      <BiChevronLeft size={40} color="blue" /> {/* Custom icon */}
+      <BiChevronLeft size={30} color="blue" />
     </div>
   );
 };
@@ -48,23 +57,26 @@ const Slider = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 3, // Default number of slides to show
+    speed: 150, // Fast transition speed for "appear"
+    slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: <PrevArrow />, // Assign custom left arrow
+    autoplay: true,
+    autoplaySpeed: 2000, // Longer stay duration
+    cssEase: "ease-in", // Start fast, ease into position
+    prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
     responsive: [
       {
-        breakpoint: 390, // Screen width below 350px
+        breakpoint: 390,
         settings: {
-          slidesToShow: 1, // Show only 1 slide
+          slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 768, // Example for larger screens (you can add more breakpoints)
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2, // Show 2 slides on medium-sized screens
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
@@ -72,26 +84,24 @@ const Slider = () => {
   };
 
   return (
-    <>
-      <section className="hero">
-        <div className="min-h-48">
-          <SliderSlick {...settings}>
-            <div>
-              <img src={assets.slider1} className="min-h-44" />
-            </div>
-            <div>
-              <img src={assets.slider2} className="min-h-44" />
-            </div>
-            <div>
-              <img src={assets.slider3} className="min-h-44" />
-            </div>
-            <div>
-              <img src={assets.slider4} className="min-h-44" />
-            </div>
-          </SliderSlick>
-        </div>
-      </section>
-    </>
+    <section className="hero">
+      <div className="min-h-48">
+        <SliderSlick {...settings}>
+          <div>
+            <img src={assets.slider1} alt="Slide 1" className="min-h-44" />
+          </div>
+          <div>
+            <img src={assets.slider2} alt="Slide 2" className="min-h-44" />
+          </div>
+          <div>
+            <img src={assets.slider3} alt="Slide 3" className="min-h-44" />
+          </div>
+          <div>
+            <img src={assets.slider4} alt="Slide 4" className="min-h-44" />
+          </div>
+        </SliderSlick>
+      </div>
+    </section>
   );
 };
 
