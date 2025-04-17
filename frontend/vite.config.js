88a19a3f40b11874c02e22438fc18fe1f0ba3f07
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2025-04-17 23:20:04 +0530

    fixed the beurer partnership trial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
-})
+  server: {
+    host: "0.0.0.0", // Ensure this is correct for binding to all IP addresses
+    port: 4173, // Or another available port
+  },
+});
