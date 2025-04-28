ccf42f9da5f5db19a7a69660e320fd7d4aacd36a
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2025-04-28 22:48:23 +0530

    added the card

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ connectCloudinary();
 // Allowed origins
 const allowedOrigins = [
   "https://sweethome-store.com",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
+  // "http://localhost:3000",
+  // "http://127.0.0.1:3000",
 ];
 
 // CORS options
