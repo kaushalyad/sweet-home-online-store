f027b31cc06f246eec49179bd2ec89ec64c0baf7
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2025-12-23 21:06:03 +0530

    Add sendEmail export to emailService

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { backendUrl, currency } from "../App";
+import { backendUrl, currency } from "../config";
 import { toast } from "react-toastify";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
